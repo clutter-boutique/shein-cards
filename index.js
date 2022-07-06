cfda95bf6e23be3c44c1601f45079141a829b3ac
Clutter Boutique <102188982+clutter-boutique@users.noreply.github.com>
2022-07-06 09:55:25 -0400

    Add Static Site,  serve trello assets

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
+import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
 import { endsWith } from 'lodash';
 import SheinProduct from './lib/SheinProduct.js';
 
-addEventListener('fetch', (event) => { event.respondWith(handleRequest(event.request)); });
+addEventListener('fetch', (event) => { event.respondWith(handleRequest(event)); });
 
-async function handleRequest(request) {
+async function handleRequest(event) {
+    const request = event.request
     const requestURL = request.url.replaceAll('%3A', ":").replaceAll('%2F', '/')
     const requestPath = requestURL.replaceAll(/https:\/\/.*?\//gi, '')
     if (requestURL.endsWith('png')) return serveImage(requestURL)
-    if (requestPath.startsWith('trello')) return serveTrello(requestURL)
+    if (requestPath.startsWith('trello')) return serveTrello(event)
     if (requestPath.startsWith('productInfo')) return getProductInfo(requestURL)
     return new Response(`URL ${request.url} not found.`, { status: 404 });
-}
-
-async function getProductInfo(requestURL) {
-    let product = new SheinProduct(requestURL.split('productPage=')[1])
-    await product.loadFromUrl()
-    return new Response(product.json, { headers: { 'Content-type': 'application/json' } })
-}
-
-async function serveImage(request) {
-    const url = new URL(request.url);
-    const key = url.pathname.split("/").pop()
-    const object = await IMAGES.get(key);
-    if (object === null) {
-        return new Response('Object Not Found', { status: 404 });
-    }
-    const headers = new Headers();
-    object.writeHttpMetadata(headers);
-    headers.set('etag', object.httpEtag);
-    return new Response(object.body, {});
 }
