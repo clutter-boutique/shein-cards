32998a846b9d2edfdd2730e9da30ea9711322cf0
Clutter Boutique <102188982+clutter-boutique@users.noreply.github.com>
2022-07-06 11:35:34 -0400

    microformt

--- public/trello/client.js
+++ public/trello/client.js
@@ -2,7 +2,7 @@
 
 var Promise = TrelloPowerUp.Promise;
 
-var ROCKET_ICON = 'https://shein-cards.faitaccompli.workers.dev/icon.png';
+var ROCKET_ICON = 'https://basicbet.ch/icon.png';
 
 TrelloPowerUp.initialize({
     'card-from-url': function(t, options) {
@@ -10,9 +10,15 @@ TrelloPowerUp.initialize({
             console.log('t is', t)
             console.log('options are', options)
             resolve({
-                name: '💻 ' + options.url + ' 🤔',
+                name: "Wondering why this doesn't work",
                 desc: 'This Power-Up knows cool things about the attached url'
             });
         });
+    },
+    'card-badges': function(t, options) {
+        return {
+            text: "Static",
+            icon: ROCKET_ICON, // for card front badges onlycolor: null,}
+        }
     }
 });
